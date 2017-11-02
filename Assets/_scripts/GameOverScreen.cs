07b0f6d548c4737acdfebc16d197ec4b96e84884
Omkar Moghe <omoghe@umich.edu>
2017-11-02 18:30:56 -0400

    game over script

---
+++ Assets/_scripts/GameOverScreen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void Restart() {
+		Debug.Log("restart game");
+		SceneManager.LoadScene("main_scene");
+	}
+
+	public void Exit() {
+		Debug.Log("exit game");
+		Application.Quit();
+	}
+}
