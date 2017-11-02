6496a6a0a66215df257782ba56526e48468271a1
Kevin <kevwolf@umich.edu>
2017-11-01 22:43:17 -0400

    Init commit

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+public class playerMovement : MonoBehaviour {
+
+	public int playerNum = 0;
+
+	public float movementSpeed;
+	public float jumpSpeed;
+
+	private Rigidbody rb;
+	private InputDevice inputDevice;
+
+	// Use this for initialization
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		inputDevice = InputManager.Devices[playerNum];
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (inputDevice == null) {
+			return;
+		}
+
+		var horizontal = inputDevice.LeftStickX;
+		rb.velocity = Vector3.left * horizontal * movementSpeed;
+
+	}
+
+
+}
