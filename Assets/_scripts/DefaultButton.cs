8a3058dec678078f091164fe3d687df34ef1013c
Omkar Moghe <omoghe@umich.edu>
2017-11-02 19:38:51 -0400

    controllers working in scenes

---
+++ Assets/_scripts/DefaultButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DefaultButton : MonoBehaviour {
+
+	EventSystem eventSystem;
+
+	// Use this for initialization
+	void Start () {
+		eventSystem = EventSystem.current;
+		StartCoroutine (HighlightButton ());
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	// https://answers.unity.com/questions/1159573/eventsystemsetselectedgameobject-doesnt-highlight.html
+	IEnumerator HighlightButton ()
+     {
+         eventSystem.SetSelectedGameObject (null);
+         yield return new WaitForEndOfFrame ();
+         eventSystem.SetSelectedGameObject (gameObject);
+     }
+}
