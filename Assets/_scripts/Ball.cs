36a5efb67404f57a4956cba6fd1d14131aaac640
Sam Adams <samharad@umich.edu>
2017-11-04 16:45:11 -0400

    Improved 2d version; added dash ability

--- Assets/_scripts/Ball.cs
+++ Assets/_scripts/Ball.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
+    private float maxSpeed = 20;
+
+    private Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (rb.velocity.magnitude > maxSpeed) {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+
 	}
 
     private void OnCollisionEnter(Collision collision)
--- Assets/_scripts/playerMovement.cs
+++ Assets/_scripts/playerMovement.cs
@@ -8,10 +8,15 @@ public class playerMovement : MonoBehaviour {
 	public int playerNum = 0;
 
 	private static float movementSpeed = 10;
-	private static float jumpSpeed = 30;
+	private static float jumpSpeed = 15;
+    private static float dashSpeed = 35;
+    private static float dashTime = .1f;
 
 	private Rigidbody rb;
 	private InputDevice inputDevice;
+
+    private bool dashing = false;
+    private bool dashed = false;
 
 	// Use this for initialization
 	void Start()
@@ -27,19 +32,39 @@ public class playerMovement : MonoBehaviour {
 			return;
 		}
 
+        if (transform.position.y < 1.51)
+        {
+            dashed = false;
+        }
+
 		var xMovement = inputDevice.LeftStickX;
         var yMovement = inputDevice.LeftStickY;
         var vertical = rb.velocity.y;
-        rb.velocity = new Vector3(xMovement * movementSpeed, vertical, yMovement * movementSpeed);
-		//rb.velocity = Vector2.right * horizontal * movementSpeed;
+
+        if (!dashing) {
+            rb.velocity = new Vector3(xMovement * movementSpeed, vertical, 0);   
+        }
 
 		var jump = inputDevice.Action1.WasPressed;
+
         if (jump && transform.position.y < 1.51) {
             rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
-		}
+        } else if (jump && !dashed) {
+            Vector2 direction = new Vector2(xMovement, yMovement);
+            StartCoroutine(Dash(direction));
+        }
 			
 
 	}
+
+    IEnumerator Dash(Vector2 direction) {
+        dashed = true;
+        dashing = true;
+        rb.velocity = direction * dashSpeed;
+        yield return new WaitForSeconds(dashTime);
+        rb.velocity = Vector2.zero;
+        dashing = false;
+    }
 
 
 }
