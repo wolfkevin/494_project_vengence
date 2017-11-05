b54505505e3c95f050ac073c22501a12737487f3
Daniel Yeomans <dyeomans@umich.edu>
2017-11-05 14:45:23 -0500

    Added eye Prefab to DanOmkarScene

---
+++ Assets/_scripts/FollowBall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowBall : MonoBehaviour {
+
+	public Transform ball;
+	public float eyeRadius = 0.01f;
+	Vector3 pupilPosition;
+
+	// Use this for initialization
+	void Start () {
+		pupilPosition = this.transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 lookDirection = (ball.position - this.transform.position).normalized;
+		this.transform.localPosition = pupilPosition + (lookDirection * eyeRadius);
+	}
+}
