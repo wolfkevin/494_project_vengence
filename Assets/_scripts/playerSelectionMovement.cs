34c4aa908baab0f7d56c886bf69c924f008a0898
Omkar Moghe <omoghe@umich.edu>
2017-11-05 15:38:24 -0500

    selection screen

--- Assets/_scripts/playerSelectionMovement.cs
+++ Assets/_scripts/playerSelectionMovement.cs
@@ -3,34 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using InControl;
 
-public class playerSelectionMovement : MonoBehaviour {
-
-	Rigidbody rb;
-
+public class PlayerSelectionMovement : MonoBehaviour {
 	public int playerNum = 0;
-	public float movementSpeed;
+
 	private InputDevice inputDevice;
 	private GameObject checkbox;
 
 	// Use this for initialization
 	void Start () {
-		rb = this.GetComponent<Rigidbody> ();
 		inputDevice = InputManager.Devices[playerNum];
-		checkbox = GameObject.FindGameObjectWithTag ("checkbox");
-		checkbox.SetActive (false);
+
+		checkbox = FindCheckbox();
+		if (checkbox != null) checkbox.GetComponent<SpriteRenderer>().enabled = false;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+		if (inputDevice == null) return;
 
-		var horizontal = inputDevice.LeftStickX;
-		var vertical = inputDevice.LeftStickY;
-		rb.velocity = new Vector3(1f * horizontal * movementSpeed, 1f * vertical * movementSpeed);
-
-		var ready = inputDevice.Action1.WasPressed;
-		if (ready) {
-			checkbox.SetActive (true);
+		// Y button to ready up
+		if (inputDevice.Action4.WasPressed) {
+			Debug.Log("Y pressed");
+			if (checkbox != null) checkbox.GetComponent<SpriteRenderer>().enabled = false;
 		}
+	}
+
+	GameObject FindCheckbox() {
+		foreach (Transform child in transform) {
+			if (child.gameObject.tag == "checkbox") {
+				return child.gameObject;
+			}
+		}
+
+		return null;
 	}
 
 }
