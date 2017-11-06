76f1361d1287f529cf760a6c88fce1cfe97f4be6
Sam Adams <samharad@umich.edu>
2017-11-05 19:31:49 -0500

    build is still unplayably slow

--- Assets/_scripts/GameManager.cs
+++ Assets/_scripts/GameManager.cs
@@ -29,7 +29,7 @@ public class GameManager : MonoBehaviour {
         {
             Destroy(gameObject);
         }
-        DontDestroyOnLoad(transform.gameObject);
+        //DontDestroyOnLoad(transform.gameObject);
     }
 
 
