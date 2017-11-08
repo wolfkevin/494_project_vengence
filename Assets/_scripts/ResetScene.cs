182362e45b52192685557662ed153ab233b522d9
Kevin <kevwolf@umich.edu>
2017-11-08 11:15:33 -0500

    Reset scene script

---
+++ Assets/_scripts/ResetScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetScene : MonoBehaviour {
+	
+	// Update is called once per frame
+	public void resetScene () {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+}
