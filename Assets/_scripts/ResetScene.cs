5926e50d6bb9b2a27b8ab7b1ab4e2ee8c374a257
Kevin <kevwolf@umich.edu>
2017-11-08 11:44:05 -0500

    New reset script

--- Assets/_scripts/ResetScene.cs
+++ Assets/_scripts/ResetScene.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ResetScene : MonoBehaviour {
-	
+
 	// Update is called once per frame
 	public void resetScene () {
 		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
