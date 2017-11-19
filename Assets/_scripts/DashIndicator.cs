a9764b0768fb4d69702d27bc0244ceb4b0178f5b
Kevin <kevwolf@umich.edu>
2017-11-19 13:05:06 -0500

    Fixed dash indicator

--- Assets/_scripts/DashIndicator.cs
+++ Assets/_scripts/DashIndicator.cs
@@ -12,6 +12,8 @@ public class DashIndicator : MonoBehaviour {
 	GameObject directionalArrow;
 
 	private InputDevice inputDevice;
+	private float xInput;
+	private float yInput;
 
 	// Use this for initialization
 	void Start () {
@@ -19,15 +21,15 @@ public class DashIndicator : MonoBehaviour {
 		directionalArrow = this.transform.GetChild (0).gameObject;
 		directionalArrow.SetActive(false);
 
-		inputDevice = transform.parent.gameObject.GetComponent<PlayerInputDevice>().inputDevice;
+		inputDevice = GetComponentInParent<PlayerInputDevice>().inputDevice;
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (pm.IsCharging ()) {
 			directionalArrow.SetActive (true);
-			var xInput = inputDevice.LeftStickX;
-			var yInput = inputDevice.LeftStickY;
+			xInput = inputDevice.LeftStickX;
+			yInput = inputDevice.LeftStickY;
 			directionalArrow.transform.localRotation = Quaternion.AngleAxis(Mathf.Atan2(yInput, xInput) * Mathf.Rad2Deg - 90, Vector3.forward);
 			directionalArrow.transform.localPosition = new Vector3 (xInput * bodyOffsetForDirectionalArrow, yInput * bodyOffsetForDirectionalArrow, 0);
 		} else {
