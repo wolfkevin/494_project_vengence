241d626fd81c622561b1a35cfd8b43e7a7931917
Kevin <kevwolf@umich.edu>
2017-11-19 11:55:44 -0500

    Moved pupil dash indicator to be on pupil object

---
+++ Assets/_scripts/PupilDashIndicator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PupilDashIndicator : MonoBehaviour {
+
+	private Vector3 pupilScale;
+	private Vector3 pupilHomePosition;
+	private FollowBall followBall;
+
+	// Use this for initialization
+	void Start () {
+
+		pupilScale = this.transform.localScale;
+		pupilHomePosition = this.transform.localPosition;
+		followBall = this.GetComponent<FollowBall>();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+
+	public void ResetPupil() {
+			this.transform.localScale = pupilScale;
+			followBall.ResumeFollowBall();
+	}
+
+	public void GrowPupil() {
+		transform.localPosition = pupilHomePosition;
+			this.transform.localScale *= 1.015f;
+	}
+
+}
