b28e3264070e5b5b8126879a0551a8f2139c8b23
Kevin <kevwolf@umich.edu>
2017-11-20 09:38:54 -0500

    Added glass breaking sound effect

--- Assets/_scripts/BreakWall.cs
+++ Assets/_scripts/BreakWall.cs
@@ -4,27 +4,25 @@ using UnityEngine;
 
 public class BreakWall : MonoBehaviour {
 
-	public float force;
+	private float force;
 	private ReadyController rc;
 	private Transform grandfather;
+	private AudioSource glassBreakingSound;
 
 
 	void Start(){
 		grandfather = this.transform.parent.parent.parent;
 		rc = GameObject.FindGameObjectWithTag ("#ReadyController").GetComponent<ReadyController>();
+		glassBreakingSound = GetComponentInParent<AudioSource>();
 	}
 
 	void OnCollisionEnter(Collision collision){
 		Rigidbody rb = collision.gameObject.GetComponent<Rigidbody> ();
-//		if (collision.rigidbody)
-//			otherMass = collision.rigidbody.mass;
-//		else 
-//			otherMass = 1000; // static collider means huge mass'
-//		collision.r
 		force = collision.relativeVelocity.magnitude * rb.mass;
 		if (force > 40f){
+			glassBreakingSound.Play();
 			Destroy(this.transform.parent.gameObject);
-			grandfather.GetComponentInChildren<Checkbox> ().SetCheckboxImage (true);
+			grandfather.GetComponentInChildren<Checkbox>().SetCheckboxImage (true);
 			rc.SetReady (grandfather.GetComponent<PlayerSelection> ().GetPlayerNum ());
 		}
 	}
