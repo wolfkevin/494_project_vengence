75eb3f0ca6bfa07bc1285358de0dca0b57fb8a3e
Sam Adams <samharad@umich.edu>
2017-11-26 15:53:35 -0500

    Fixed selection screen

---
+++ Assets/_scripts/MakeUnstuck.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeUnstuck : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    private float unstickThreshold = 0.1f;
+    private float unstickVelocity = 10;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionStay(Collision collision)
+    {
+        var velocity = rb.velocity;
+         
+        if (Mathf.Abs(velocity.x) < unstickThreshold) {
+            velocity.x += unstickVelocity;
+        }
+        if (Mathf.Abs(velocity.y) < unstickThreshold) {
+            velocity.y += unstickVelocity;   
+        }
+
+        rb.velocity = velocity;
+    }
+
+
+}
