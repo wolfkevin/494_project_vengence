74061b86f8cc7ebf747e3531442ed2f3e8223752
Omkar Moghe <omoghe@umich.edu>
2017-11-26 15:01:57 -0500

    win by 2 check

--- Assets/_scripts/FinalScoreDisplayer.cs
+++ Assets/_scripts/FinalScoreDisplayer.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class FinalScoreDisplayer : MonoBehaviour {
@@ -8,8 +6,8 @@ public class FinalScoreDisplayer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		title = (Text) GetComponent<Text>();
-		title.text = GameData.LeftSideScore.ToString() + " - " + GameData.RightSideScore.ToString();
+		title = GetComponent<Text>();
+		title.text = GameData.LeftSideScore + " - " + GameData.RightSideScore;
 	}
 
 	// Update is called once per frame
--- Assets/_scripts/GameData.cs
+++ Assets/_scripts/GameData.cs
@@ -3,24 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public static class GameData {
-    private static int leftSideScore;
-    private static int rightSideScore;
+    public static int LeftSideScore { get; set; }
 
-    public static int LeftSideScore {
-        get {
-            return leftSideScore;
-        }
-        set {
-        leftSideScore = value;
-        }
-    }
-
-	public static int RightSideScore {
-        get {
-            return rightSideScore;
-        }
-        set {
-            rightSideScore = value;
-        }
-    }
+    public static int RightSideScore { get; set; }
 }
