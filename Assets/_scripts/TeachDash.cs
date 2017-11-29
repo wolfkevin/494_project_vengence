ed3a66b25737eab373d48f98d8eb31a5474f443f
Kevin <kevwolf@umich.edu>
2017-11-29 16:31:57 -0500

    Teach player

---
+++ Assets/_scripts/TeachDash.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeachDash : MonoBehaviour {
+
+	JumpInstruction jumpInstruction;
+	DashInstruction dashInstruction;
+
+	// Use this for initialization
+	void Start () {
+		jumpInstruction = GetComponentInChildren<JumpInstruction>();
+		dashInstruction = GetComponentInChildren<DashInstruction>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/_scripts/TeachPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeachPlayer : MonoBehaviour {
+
+	TeachDash teachDash;
+	// GameObject teachTransform;
+
+	// Use this for initialization
+	void Start () {
+		teachDash = GetComponentInChildren<TeachDash>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void DashTaught(){
+		Destroy(teachDash.gameObject);
+	}
+}
